b4c15bab65cb176c9bc64fdc50e4b6b3b134d3bd
apple <624990742@qq.com>
2019-05-17 13:46:15 +0800

    基础的数据绑定语法

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,40 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  
+data:{
+message:'Hello MINA!',
+id:0,
+condition:true,
+a:1,
+b:2,
+c:3,
+zero: 0,
+aa:111,
+bb:222,
+name:'小贾',
+object:{
+  key:'Hello'
+},
+array:['MINA']
+},
+  
+  obj1: {
+    a: 1,
+    b: 2
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  obj2: {
+    b: 3,
+    c: 4
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  a: 5,
+
+
+  clickMe(){
+  this.setData({msg:"你好，世界"})
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
+
+
+
